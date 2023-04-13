44a053652ea940227bc7b6aa8109fbee2c3e7301
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-04-13 23:05:31 +0400

    do tack

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity ) {
+ // let activ=Number( sampleActivity);
+  if (  sampleActivity==='' ){return false}
+  else if ( isNaN(sampleActivity) ){return false}
+  else if (  Number.isInteger(sampleActivity) ){return false}
+  else if (  sampleActivity===null ){return false}
+  else if (   Number(sampleActivity)<=0 ){return false}
+  else if (  Number(sampleActivity)>15){return false}
+  else if (  !(typeof (sampleActivity) === 'string')){return false}
+  else {
+    let t= Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD);
+    return Math.ceil(t);
+  
+  }console.debug(Math.ceil(t))
 }
 
+dateSample('one')
+dateSample('')
+dateSample(' ')
+dateSample(' \n\t\r')
 module.exports = {
   dateSample
 };
