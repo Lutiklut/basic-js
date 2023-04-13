d643ad734e848cbab3d1e9740f8db65a424ca031
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-04-14 00:32:34 +0400

    do tack

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members ) {
+  if (!(Array.isArray(members))){return false}
+  else if (members===null){ return false}
+  else if (members===undefined){return false}
+  else if (members===false){return false}
+  else if (  Number(members) ){return false}
 
+
+  else{
+  let name=members.map((el)=>{
+  if (!(typeof (el) === 'string')){false}
+  else if (el===null){false}
+  else if (el===undefined){false}
+  else if (el===false){false}
+  else if (!isNaN(el)){false}
+  else if (typeof (el) === "object"){false}
+  else{
+  
+  return el.trim().toUpperCase()[0]}
+})
+    name.sort((a, b) => a.localeCompare(b));
+   return name.join('')
+  }}
 module.exports = {
   createDreamTeam
 };
