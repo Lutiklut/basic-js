7c154afd01c1fa5d809fc0b0fa05d35d0b87a03a
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-04-15 17:08:40 +0400

    do tack

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
+  let myStr='';
+  
+  console.log(typeof(str), '1')
+  //console.log(str)
+  if (typeof(str)==='object'){
+   
+   console.log(typeof(str))
+
+  }
+  
+  str=String(str);
+
+  if(!options.addition===null||!options.addition===undefined||!typeof(options.addition)!='string'){options.addition=String(options.addition);}
+  console.log(typeof(options.addition))
+  if( typeof str === 'string'){
+    if(!options){return str}
+      else if(!options.separator&&!options.additionSeparator){
+        return   str.concat((options.addition.concat(('|')).repeat(options.additionRepeatTimes)).slice(0,-1)).concat('+').repeat(options.repeatTimes).slice(0,-1)
+      }
+
+      else if(!options.repeatTimes){
+        if(!options.additionRepeatTimes){return myStr=str.concat((options.addition)).concat(options.separator).slice(0,-options.separator.length);
+        }
+        else{return myStr=str.concat((options.addition.concat((options.additionSeparator)).repeat(options.additionRepeatTimes)).slice(0,-options.additionSeparator.length)).concat('options.separator').slice(0,-options.separator.length);
+      }}
+    else if(!options.addition){
+        if (!options.separator){return myStr=str.concat('+').repeat(options.repeatTimes).slice(0,-1)}
+        else { return myStr=str.concat(options.separator).repeat(options.repeatTimes).slice(0,-options.separator.length)}}
+    else if(!options.separator){ 
+      if(!options.additionSeparator){return myStr=str.concat('+').repeat(options.repeatTimes).slice(0,-1)}
+      
+      else{console.log(123);return myStr=str.concat((options.addition.concat((options.additionSeparator)).repeat(options.additionRepeatTimes)).slice(0,-options.additionSeparator.length)).concat('+').repeat(options.repeatTimes).slice(0,-1)
+    }}
+    else if(!options.additionSeparator){ return myStr=str.concat((options.addition.concat(("|")).repeat(options.additionRepeatTimes)).slice(0,-1)).concat(options.separator).repeat(options.repeatTimes).slice(0,-options.separator.length)
+    }
+    else { 
+      return myStr=str.concat((options.addition.concat((options.additionSeparator)).repeat(options.additionRepeatTimes)).slice(0,-options.additionSeparator.length)).concat(options.separator).repeat(options.repeatTimes).slice(0,-options.separator.length)
+    }}
+} 
+  
+  console.debug(repeater('REPEATABLE_STRING', { repeatTimes: 2, addition: 'ADDITION', additionSeparator: '222', additionRepeatTimes: 3 }))
+  // 'REPEATABLE_STRINGADDITION222ADDITION222ADDITION+REPEATABLE_STRINGADDITION222ADDITION222ADDITION');
+  // 'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT');
+
+
 
 module.exports = {
   repeater
