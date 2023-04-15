3601b791cd14d892611c82e11d9cabd385d65658
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-04-15 22:55:06 +0400

    do tack

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -22,17 +22,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+  constructor(cod) {
+    this.words = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    this.cod = cod || cod === undefined;
+  }
   encrypt(message,key) {
     if(!message||!key){throw new Error('Incorrect arguments!')}
-    //throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let index = 0;
+    let res = "";
+    message = message.toUpperCase();
+    key  = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.words.includes(message[i])) {
+        res += message[i];
+        continue;
+      }
+      const newMessage = this.words.indexOf(message[i]);
+      const codeKey = this.words.indexOf(key[index++ % key.length]);
+      const encryptCode = (newMessage + codeKey) % this.words.length;
+      res += this.words[encryptCode];
+    }
+    return this.cod ? res : res.split('').reverse().join('');
+
+	}
+
   decrypt(encryptedMessage,key) {
-   // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
     if(!encryptedMessage||!key){throw new Error('Incorrect arguments!')}
+
+    let index = 0;
+    let res = "";
+    
+    key=key.toUpperCase();
+    encryptedMessage=encryptedMessage.toUpperCase();
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (!this.words.includes(encryptedMessage[i])) {
+        res += encryptedMessage[i];
+        continue;
+      }
+
+      const code = this.words.indexOf(encryptedMessage[i]);
+      const codeKey = this.words.indexOf(key[index++ % key.length]);
+      const deCode = code - codeKey % this.words.length;
+
+      if (deCode < 0)
+      res += this.words[this.words.length + deCode];
+      else
+      res += this.words[deCode];
+    }
+    return this.cod ? res : res.split('').reverse().join('');
   }
 }
+
+     
+
 
 module.exports = {
   VigenereCipheringMachine
