b0f35b7015894f2944f151da73d60f5d29bb1a03
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-04-15 18:01:01 +0400

    do tack

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -12,30 +12,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
+ * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'*/
 function repeater(str, options) {
-  let myStr='';
-  
-  console.log(typeof(str), '1')
-  //console.log(str)
-  if (typeof(str)==='object'){
-   
-   console.log(typeof(str))
+  let myStr='';let a = options.repeatTimes;
 
-  }
-  
+ 
+  if (typeof(str)==='string'){
+ if(!options.separator&&!options.addition&&!options.additionSeparator&&!options.additionRepeatTimes){ 
+    return   str.concat('+').repeat(options.repeatTimes).slice(0,-1)
+  }}
+
   str=String(str);
+  
 
   if(!options.addition===null||!options.addition===undefined||!typeof(options.addition)!='string'){options.addition=String(options.addition);}
-  console.log(typeof(options.addition))
-  if( typeof str === 'string'){
+  if(!typeof(str)!='string'){options.addition=String(options.addition);}
+  //console.log(typeof(options.addition))
+  if( typeof str === 'string'){ 
+
     if(!options){return str}
+   
+    else if(!options.separator&&!options.additionSeparator&&!options.additionRepeatTimes&&options.addition){console.log(5)
+        return   str.concat(options.addition).concat('+').repeat(options.repeatTimes).slice(0,-1)
+      } 
+     
       else if(!options.separator&&!options.additionSeparator){
         return   str.concat((options.addition.concat(('|')).repeat(options.additionRepeatTimes)).slice(0,-1)).concat('+').repeat(options.repeatTimes).slice(0,-1)
       }
-
+      
       else if(!options.repeatTimes){
         if(!options.additionRepeatTimes){return myStr=str.concat((options.addition)).concat(options.separator).slice(0,-options.separator.length);
         }
@@ -49,18 +53,18 @@ function repeater(str, options) {
       
       else{console.log(123);return myStr=str.concat((options.addition.concat((options.additionSeparator)).repeat(options.additionRepeatTimes)).slice(0,-options.additionSeparator.length)).concat('+').repeat(options.repeatTimes).slice(0,-1)
     }}
-    else if(!options.additionSeparator){ return myStr=str.concat((options.addition.concat(("|")).repeat(options.additionRepeatTimes)).slice(0,-1)).concat(options.separator).repeat(options.repeatTimes).slice(0,-options.separator.length)
+    else if(!options.additionSeparator){ 
+      return myStr=str.concat((options.addition.concat(("|")).repeat(options.additionRepeatTimes)).slice(0,-1)).concat(options.separator).repeat(options.repeatTimes).slice(0,-options.separator.length)
     }
     else { 
       return myStr=str.concat((options.addition.concat((options.additionSeparator)).repeat(options.additionRepeatTimes)).slice(0,-options.additionSeparator.length)).concat(options.separator).repeat(options.repeatTimes).slice(0,-options.separator.length)
     }}
 } 
-  
-  console.debug(repeater('REPEATABLE_STRING', { repeatTimes: 2, addition: 'ADDITION', additionSeparator: '222', additionRepeatTimes: 3 }))
-  // 'REPEATABLE_STRINGADDITION222ADDITION222ADDITION+REPEATABLE_STRINGADDITION222ADDITION222ADDITION');
-  // 'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT');
-
-
+// const objWithSpecificCoercion = {
+//   [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+// };
+  console.log(('la', { repeatTimes: 3 }));//, 'la+la+la');
+//console.debug(repeater('single', { repeatTimes: 1 }))//, 'single'))
 
 module.exports = {
   repeater
